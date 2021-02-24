d987cfbb39cf2d60a23ee7a109b846610be61496
Bilal AMRI <iambilalamri@gmail.com>
2021-02-24 20:19:29 +0100

    initial commit

---
+++ src/main/java/com/amrib/spqr/SpringBootGraphqlSpqrApplication.java
@@ -0,0 +1,13 @@
+package com.amrib.spqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlSpqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlSpqrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amrib/spqr/SpringBootGraphqlSpqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrib.spqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlSpqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
